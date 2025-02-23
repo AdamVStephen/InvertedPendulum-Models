66ea2dff1dc75b0c02297d879b5ad3cf27bfc881
Adam V. Stephen (WSL U2204 L1254) <adam.stephen@ukaea.uk>
2025-02-23 18:53:21 +0000

    Initial simulation working with dual USB serial cables back2back

---
+++ invertedPendulum2.py
@@ -0,0 +1,359 @@
+#!/usr/bin/env python3
+#
+"""
+Python models of the UCLA Edukit rotary inverted pendulum
+and the associated STM32 embedded rotary encoder/motor
+sensor and actuator systems.
+"""
+
+import pdb
+
+from struct import pack, unpack, calcsize
+
+class L6474:
+    """
+    Attributes of the L6474 motor controller
+    c.f. L6474_Init_t
+    Augmented with other motor information for pretty printing.
+    """
+    def __init__(self):
+        self.instance = 1
+        self.max_acceleration = self.max_deceleration = self.max_speed = 131071
+
+
+class CommandInterpreter:
+    """
+    This class handles command packets (TX)
+    *        byte 0: requested command ID
+    *        byte 1: device ID
+    *        bytes 2-9: command parameters
+    """
+    # commandID: ["Description", pack format]
+    command_encoding = {
+        17 : ["GoTo Location", 10, 'BBI4b'],
+        22 : ["Set Acceleration", 10, 'BBI4b'],
+        252 : ["Reset.  No parameters", 10, 'BB7b'],
+        253 : ["Read Status. No parameters", 10,  'BB7b' ],
+        254 : ["Apply acceleration", 10, 'BBff']
+    }
+
+    def __init__(self):
+        pass
+
+    def handle_command(self, commandID):
+        if commandID:
+            pass
+
+CMDERR_OK = 0
+CMDERR_INVALID_COMMAND = 1
+CMDERR_INVALID_DEVICE = 2
+
+class ResponseInterpreter:
+    """
+ * @brief Response (13 bytes) has the following structure:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2: error code (0 == OK)
+ *        bytes 3-6: command return values
+ *        or
+ *        bytes 3-12: status
+
+ To distinguish which type of response has been sent we use the size
+ """
+    response_decoding = {
+        # commandID: ["Description", size, format]
+        252 : ["Reset", 7, 'bbb?bbb'],  # Single bool ret = BSP_MotorControl_SoftStop() trailing packet data for no reason
+        253 : ["Read status", 13, 'bbbbbiI'],
+        254 : ["Apply acceleration", 0, ''] # No response is sent
+    }
+
+
+
+from collections import namedtuple
+
+class SerialFrame:
+    """
+    Base class for describing serial protocols
+    """
+    format = "!b"
+    fields = [
+        ('demo_byte', 0),
+        ]
+
+    def __init__(self):
+        (field_names, default_values) = zip(*self.fields)
+        self.Record = namedtuple('Record', ' '.join(field_names), defaults = default_values)
+        self.record = self.Record()
+
+    def unpack(self, data):
+        self.record = self.record._make(unpack(self.format, data))
+
+    def pack(self, *args):
+        if len(args) != len(self.format) - 1:
+            raise RuntimeError(f'{len(args)} arguments provided, {len(self.format) -1} arguments required')
+
+        self.data = pack(self.format, *args)
+
+        self.unpack(self.data)
+
+    def describe(self):
+        return(f"Frame format {self.format} contains {calcsize(self.format)} bytes")
+
+    def __repr__(self):
+        r = []
+        r.append(f'{self.__class__.__name__} with format {self.format}')
+        for field in self.record._fields:
+            r.append("%s : %s" % (field, self.record.__getattribute__(field)))
+        r.append(self.describe())
+        return '\n'.join(r)
+
+
+class CommandFrame(SerialFrame):
+    """
+    CommandFrame according to RokHari control implementation
+    Commands may have varying number of command parameters
+
+    No table of consistency is provided - the decoder in
+    the STM32 code provides the lookup.
+
+    The decoder deals with unpacking 1,2,4 byte structures
+    and serializing to C types.
+
+    In this python support tool, we implement the deserialization
+    for the commands used in the MARTe2 control application.
+
+    TODO: code review - check if the UCLA project and L6474 driver
+    provide any mapping on commandID to API.
+
+    DeviceID comes from L6474 design which can multiplex  boards.
+
+    Command frame layout:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2-9: command parameters
+
+    """
+
+    format = "!BBQ"
+    fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('params', 0),
+            ]
+
+    def __init__(self, param_spec = None):
+        # Option to customise the class on the fly
+        # param_spec must be a list of (field, format, default_value)
+        if param_spec is not None:
+            fields = self.fields[0:2]
+            format = self.format[0:3]
+            for (field_name, field_format, field_default) in param_spec:
+                format += field_format
+                fields.append((field_name, field_default))
+            self.format = format
+            self.fields = fields
+        super().__init__()
+
+class CommandFrame(SerialFrame):
+    """
+    """
+
+    format = "!BBQ"
+    fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('params', 0),
+            ]
+
+
+class CommandResponseFrame(SerialFrame):
+    """
+    ReponseFrame according to RokHari control implementation
+
+ * @brief Response (13 bytes) has the following structure:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2: error code (0 == OK)
+ *        bytes 3-6: command return values : depend on command
+   """
+
+    format = "!bbbI"
+    fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('errorCode', 0),
+            ('cmdResponse', 0),
+            ]
+
+
+class StatusResponseFrame(SerialFrame):
+    """
+    ReponseFrame according to RokHari control implementation
+
+ * @brief Response (13 bytes) has the following structure:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2: error code (0 == OK)
+ *        bytes 3-6: command return values
+ *        or
+ *        bytes 3-12: status
+
+    Which response is received can be determined by frame size (7 or 13)
+
+    This class is for the status response frame
+   """
+    format = "!bbbbbiI"
+    fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('errorCode', 0),
+            ('responseSize', 13),
+            ('motorState', 0),
+            ('motorPos', 0),
+            ('encoderPos', 0)
+            ]
+
+    def pack(self, commandID, deviceID, errorCode, responseSize, motorState, motorPos, encoderPos):
+        """Repeat the field order to give better function signature"""
+        self.data = pack(self.format, commandID, deviceID, errorCode, responseSize,
+                            motorState, motorPos, encoderPos)
+        self.unpack(self.data)
+
+class DataFrame(SerialFrame):
+    """
+    DataFrame model from RokHari control implementation
+     Status response data layout:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2: error code (0 == OK)
+ *        byte 3: total response size (13)
+ *        byte 4: motorState_t
+ *        bytes 5-8: motor position (int32_t)
+ *        bytes 9-12: encoder position (uint32_t)
+    """
+
+    format = "!bbbbbiI"
+    fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('errorCode', 0),
+            ('responseSize', 13),
+            ('motorState', 0),
+            ('motorPos', 0),
+            ('encoderPos', 0)
+            ]
+
+    def pack(self, commandID, deviceID, errorCode, responseSize, motorState, motorPos, encoderPos):
+        """Repeat the field order to give better function signature"""
+        self.data = pack(self.format, commandID, deviceID, errorCode, responseSize,
+                            motorState, motorPos, encoderPos)
+        self.unpack(self.data)
+
+
+class DataFrame0:
+    """
+    DataFrame model from RokHari control implementation
+     Status response data layout:
+ *        byte 0: command ID
+ *        byte 1: device ID
+ *        byte 2: error code (0 == OK)
+ *        byte 3: total response size (13)
+ *        byte 4: motorState_t
+ *        bytes 5-8: motor position (int32_t)
+ *        bytes 9-12: encoder position (uint32_t)
+    """
+    format = "!bbbbbiI"
+    def __init__(self):
+        self.fields = [
+            ('commandID', 0),
+            ('deviceID', 0),
+            ('errorCode', 0),
+            ('responseSize', 13),
+            ('motorState', 0),
+            ('motorPos', 0),
+            ('encoderPos', 0)
+            ]
+        (field_names, default_values) = zip(*self.fields)
+        self.Record = namedtuple('Record', ' '.join(field_names), defaults = default_values)
+        self.record = self.Record()
+
+    def unpack(self, data):
+        self.record = self.record._make(unpack(self.format, data))
+
+    def pack(self, commandID, deviceID, errorCode, responseSize, motorState, motorPos, encoderPos):
+
+        self.data = pack(self.format, commandID, deviceID, errorCode, responseSize,
+                            motorState, motorPos, encoderPos)
+
+        self.unpack(self.data)
+
+    def __repr__(self):
+        r = []
+        r.append(f'{self.__class__.__name__} with format {self.format}')
+        for field in self.record._fields:
+            r.append("%s : %d" % (field, self.record.__getattribute__(field)))
+        return '\n'.join(r)
+
+dataFrameTestData = {}
+
+
+def ut_SerialFrame():
+    sf = SerialFrame()
+    try:
+        sf.pack(1,2)
+    except RuntimeError:
+        print("Overpacking serial frame exception detected")
+    try:
+        sf.pack(127)
+    except RuntimeError:
+        print("Fault : this should work")
+    else:
+        print(sf)
+
+def ut_DataFrame0():
+    df = DataFrame0()
+    df.pack(1,2,0,13,10,11,100)
+    print(df)
+    data = pack(DataFrame.format, 10, 20, 0, 13, 20, 22, 200)
+    dg = DataFrame0()
+    dg.unpack(data)
+    print(dg)
+
+def ut_DataFrame():
+    df = DataFrame()
+    df.pack(1,2,0,13,10,11,100)
+    print(df)
+    data = pack(DataFrame.format, 10, 20, 0, 13, 20, 22, 200)
+    dg = DataFrame()
+    dg.unpack(data)
+    print(dg)
+
+def ut_CommandResponseFrame():
+    crf = CommandResponseFrame()
+    crf.pack(1,2,3,4)
+    print(crf)
+
+def ut_StatusResponseFrame():
+    srf = StatusResponseFrame()
+    srf.pack(1,2,3,13,5,6,7)
+    print(srf)
+
+class MockEdukitSTM32:
+    def __init__(self):
+        pass
+
+class MockEdukitMARTe2:
+    def __init__(self):
+        pass
+
+
+
+def main():
+    ut_DataFrame0()
+    ut_SerialFrame()
+    ut_DataFrame()
+    ut_CommandResponseFrame()
+    ut_StatusResponseFrame()
+
+if __name__ == '__main__':
+    main()
--- ip.py
+++ ip.py
@@ -5,18 +5,274 @@ Python models of the UCLA Edukit rotary inverted pendulum
 and the associated STM32 embedded rotary encoder/motor 
 sensor and actuator systems
 """
+import pdb
+import serial
+import sys
 
-from invertedPendulum import CommandFrame, ResponseFrame
+from struct import calcsize
+from time import sleep, time
+from invertedPendulum import CommandFrame, CommandResponseFrame, StatusResponseFrame
 
-class ActiveStatusMonitor:
+def unhexlify(s):
+    s = re.sub(r"[^0-9a-fA-F]", "", s)
+    return binascii.unhexlify(s)
+
+def hexlify(s):
+    return " ".join(bytes([c]).hex().upper() for c in s)
+
+class SerialPort:
+    def __init__(self, port = '/dev/ttyUSB0', baud = 230400, verbose = False):
+        self.port = port
+        #self.baud = 9600
+        #self.baud = 115200
+        self.baud = baud
+        self.timeout = 10
+        self.ser = serial.Serial(port = self.port, baudrate = self.baud)
+        #pdb.set_trace()
+        self.ser.timeout = 1
+        self.iterations = 5
+        self.verbose = verbose
+        sleep(0.2)
+
+    def write(self, bytes):
+        txb = self.ser.write(bytes)
+        self.ser.flush()
+        # TODO: apply logging, not print
+        if self.verbose:
+            tx_bytes = hexlify(bytes)
+            print(f"TX {txb:03d} {tx_bytes:<60}")
+        return txb
+    
+    def read(self, rxsize):
+        rxb = 0
+        rxbuf = bytearray()
+        while rxb < rxsize and self.ser.inWaiting() > 0:
+            rxbuf.extend(self.ser.read(1))
+            rxb+=1
+        if self.verbose:
+            print(f"RX {rxb:03d} {hexlify(rxbuf):<60}")
+        return rxbuf
+
+    def echo_test(self):
+        iterations = self.iterations
+        ipsm = InvPendStatusMonitor()
+        buf = bytearray()
+        out = ipsm.reset_cmd.data
+        while iterations > 0:
+            txb = self.ser.write(out)
+            self.ser.flush()
+            print(f"TX : sent {txb} bytes")
+            sleep(0.01)
+            #pdb.set_trace()d
+            while self.ser.inWaiting() > 0:
+                buf.extend(self.ser.read(1))
+            if len(buf) > 0:
+                print("Sent    : %s" % hexlify(out))
+                print("Received: %s" % hexlify(buf))
+            else:
+                print(f"No data received {iterations}\n")
+            iterations-=1
+
+
+class SerialTransmitter:
+    def __self__(init):
+        pass
+
+
+class SerialReceiver:
+    def __self__(init):
+        pass
+
+class MockEdukitSTM32:
+    """
+    An object to model the behaviour of the EdukitSTM32 
+    """
+    def __init__(self, port = '/dev/ttyUSB2', baud = 230400, verbose = False):
+        self.serial_port = SerialPort(port, baud)
+        self.verbose = verbose
+        self.status_response = StatusResponseFrame()
+        self.command = CommandFrame()
+
+    def handle_commands(self):
+        loops = 0
+        handled = 0
+        while True:
+            loops+=1
+            expected_bytes = calcsize(self.command.format)
+            rx_data = self.serial_port.read(expected_bytes)
+            commandID = None
+#            pdb.set_trace()
+            if len(rx_data) != expected_bytes:
+                log_msg=f"Mock : {len(rx_data)} data != {expected_bytes}: sleep(1) {loops:<10d}"
+                sleep(1)
+            else:
+                self.command.unpack(rx_data)
+                commandID = self.command.record.commandID
+                handled+=1
+                log_msg=f"Mock : command {commandID}: sleep(1) {handled}/{loops:<10d}"
+            if self.verbose: print(log_msg)
+            if commandID is not None:
+                if commandID == 252:
+                    # reset command : no response
+                    pass
+                elif commandID == 253:
+                    # status command : create and send status
+                    self.status_response.pack(commandID, 1, 0, 13, 10, 20, 30)
+                    self.transceive(self.status_response)
+                    pass
+                elif commandID == 254:
+                    # apply acceleration command : no response
+                    pass
+                else:
+                    # Other command
+                    pass
+
+    def status(self):
+        status_response = StatusResponseFrame()
+        data = self.transceive(self.status_cmd, status_response)
+
+    def transceive(self, cmd, response_frame = None):
+        """
+        Send a command and read back the response.
+        This will not work in echo mode as we require a real response.
+        """
+        txb = self.serial_port.write(cmd.data)
+        if self.verbose: print(f"sent response of {txb} bytes {len(cmd.data)}")
+        if response_frame is None: return None
+        rx_data = self.serial_port.read(calcsize(response_frame.format))
+        response_frame.pack(rx_data)
+        return rx_data
+   
+    def __repr__(self):
+        r = []
+        r.append("%s" % self.reset_cmd)
+        r.append("%s" % self.reset_cmd.data)
+        return '\n'.join(r)
+
+
+class InvPendStatusMonitor:
     """
     An object to monitor the status of the system actively
     """
-    def __init__(self):
-        cmd = CommandFrame(253,1,0,)
+    def __init__(self, port = '/dev/ttyUSB0', baud = 230400):
+        self.serial_port = SerialPort(port, baud)
+        self.reset_cmd = CommandFrame()
+        self.reset_cmd.pack(252, 1, 0)
+        self.status_cmd = CommandFrame()
+        self.status_cmd.pack(253, 1, 0)
 
-def main():
-    cf = CommandFrame()
+    def reset(self):
+        self.transceive(self.reset_cmd)
+
+    def status(self):
+        status_response = StatusResponseFrame()
+        data = self.transceive(self.status_cmd, status_response)
+
+    def transceive(self, cmd, response_frame = None):
+        """
+        Send a command and read back the response.
+        This will not work in echo mode as we require a real response.
+        """
+        invalid_frames=0
+        null_frames = 0
+        txb = self.serial_port.write(cmd.data)
+        if response_frame is None: return None
+        rx_bytes = 0
+        expected_bytes = calcsize(response_frame.format)
+        while(rx_bytes == 0):
+            rx_data = self.serial_port.read(expected_bytes)
+            rx_bytes = len(rx_data)
+            if rx_bytes == 0:
+                null_frames+=1
+                sleep(1)
+                print(f"{null_frames} null frames {invalid_frames} invalid_frames")
+            elif rx_bytes == expected_bytes:
+                response_frame.unpack(rx_data)
+                print(f"decoded frame {response_frame}")
+            else:
+                invalid_frames+=1
+        return rx_data
+   
+    def __repr__(self):
+        r = []
+        r.append("%s" % self.reset_cmd)
+        r.append("%s" % self.reset_cmd.data)
+        return '\n'.join(r)
+
+def ut():
+    cfII = CommandFrame([('parA', 'I', 3.14), ('parB', 'i', 6.28)])
+    print(cfII)
+
+# Ref https://stackoverflow.com/questions/24440143/how-do-i-make-the-pyserial-loopback-work
+def serialDataPump():
+    ser = serial.serial_for_url('loop://', timeout=1)
+    testCtr = 0;
+    while True: 
+        ser.write(bytes("Test\n", encoding='ascii'))
+        time.sleep(1)
+        testCtr += 1
+
+def serialDataTestRcv():
+    ser = serial.serial_for_url('loop://', timeout=1)
+    while True: 
+        line = ser.readline()
+        sys.stdout.write('received' + str(line))
+
+def testSerMain():
+    thread1 = Thread(target = serialDataPump)
+    thread2 = Thread(target = serialDataTestRcv)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    time.sleep(2)
+
+def main_was():
+   ipsm = InvPendStatusMonitor()
+   print(ipsm) 
+   usb0 = SerialPort(115200)
+   usb0.echo_test()
+    
+
+def reset():
+    ipsm = InvPendStatusMonitor('/dev/ttyUSB1', 115200)
+    ipsm.reset()
+
+def tx_status_request():
+    ipsm = InvPendStatusMonitor('/dev/ttyUSB1', 115200)
+    ipsm.status()
+
+def mock():
+    mock = MockEdukitSTM32('/dev/ttyUSB2', 115200, verbose=True)
+    mock.handle_commands()
+
+
+def fullMockTest():
+    threadEdukit = Thread(target = mock32)
+    threadMarte2 = Thread(target = tx_status_request)
+    threadEdukit.start()
+    threadMarte2.start()
+    thread1.join()
+    time.sleep(2)
+
+def main_test():
+    usb0 = SerialPort(115200)
+    usb0.echo_test()
+
+def main(args):    # TODO: refactor the CLI
+    if len(args) == 0:
+        main_test()
+    role = args[0]
+    if role == "status":
+        tx_status_request()
+    elif role == "reset":
+        reset()
+    elif role == "mock":
+        mock()
+    elif role == "full":
+        fullMockTest()
+    else:
+        print(f"No such role: {role}")
 
 if __name__ == '__main__':
-    main()
+    #print(len(sys.argv))
+    main(sys.argv[1:])
